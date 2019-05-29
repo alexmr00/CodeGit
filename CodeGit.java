663bd4270af24b5d7fc267d9d3aac9568cfe2c8d
carlos11daw <47814157+carlos11daw@users.noreply.github.com>
2019-05-29 12:59:19 +0200

    Add files via upload

--- CodeGit.java
+++ CodeGit.java
@@ -1,21 +1,69 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package codegit;
-
 /**
  *
- * @author daw115
+ * @author DAW112
+ * @author DAW103
+ * @author DAW115
  */
-public class CodeGit {
 
+import java.util.Scanner;
+
+public class CodeGit {
+static Scanner leer = new Scanner(System.in);
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+     
+       Scanner sn = new Scanner(System.in);
+       /**
+        *@param salir salir del menú
+        */
+        boolean salir = false;
+        /**
+        *@param opcion escoger opción del menú
+        */
+        int opcion; //Guardaremos la opcion del usuario
+ 
+        while (!salir) {
+            System.out.println("Elige el tipo de conversion:");
+            System.out.println("1. lonxitude.");
+            System.out.println("2. Potencia");
+            System.out.println("3. Temperatura");
+            System.out.println("4. Salir");
+ 
+            try {
+ 
+                System.out.println("Escribe una de las opciones");
+                opcion = sn.nextInt();
+ 
+                switch (opcion) {
+                    case 1:
+                        System.out.println("Has seleccionado la opcion Conversion de Longitud");
+                        System.out.println("introduce los datos:");
+                        lonxitude();
+                        break;
+                    case 2:
+                        System.out.println("Has seleccionado la opcion Potencia");
+                            Potencia();
+                        break;
+                    case 3:
+                        System.out.println("Has seleccionado la opcion Conversion Temperatura");
+                            Temperatura();
+                        break;
+                    
+                    case 4:
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("Solo números entre 1 y 3");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Debes insertar un número");
+                sn.next();
+            }
+        }
+ 
     
-}
+        
+    }
